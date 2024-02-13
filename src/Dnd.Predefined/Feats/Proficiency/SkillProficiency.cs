52e413f88ae2a573e8ec2d8f3093cfde0fbc24e1
Harun Uyar <harunuyar@outlook.com.tr>
2024-02-13 15:36:06 +0100

    added AddLevel command and fixed skill proficiency

--- src/Dnd.Predefined/Feats/Proficiency/SkillProficiency.cs
+++ src/Dnd.Predefined/Feats/Proficiency/SkillProficiency.cs
@@ -2,6 +2,7 @@
 
 using Dnd.System.CommandSystem.Commands;
 using Dnd.System.CommandSystem.Commands.BooleanResultCommands;
+using Dnd.System.CommandSystem.Commands.EventCommands;
 using Dnd.System.CommandSystem.Commands.IntegerResultCommands;
 using Dnd.System.Entities.Skills;
 
@@ -11,30 +12,70 @@ public class SkillProficiency : AFeat
     {
         Skill = skill;
         ProficiencyType = proficiencyType;
+        IsOverridden = false;
     }
 
     public ISkill Skill { get; }
 
     public EProficiencyType ProficiencyType { get; private set; }
 
-    public override string Description => $"You are proficient at {Skill.Name} with {ProficiencyType} and will have proficiency bonus to your skill checks";
+    private bool IsOverridden { get; set; }
+
+    public override string Description => $"You are proficient at {Skill.Name} with {ProficiencyType}";
 
     public override void HandleCommand(ICommand command)
     {
-        if (command is HasSkillProficiency hasSkillProficiency && hasSkillProficiency.Skill == Skill && ProficiencyType == EProficiencyType.FullProficiency)
+        if (command is AddLevel addLevel)
         {
-            hasSkillProficiency.SetValue(this, true);
+            if (IsOverridden)
+            {
+                return;
+            }
+
+            var newSkillProficiencies = addLevel.Level.Feats
+                .Where(feat => feat is SkillProficiency skillProficiency && skillProficiency.Skill == Skill)
+                .Select(feat => (SkillProficiency)feat);
+            
+            foreach (var newSkillProficiency in newSkillProficiencies)
+            {
+                if (newSkillProficiency.ProficiencyType >= ProficiencyType)
+                {
+                    IsOverridden = true;
+                }
+                else 
+                {
+                    newSkillProficiency.IsOverridden = true;
+                }
+            }
         }
-        else if (command is HasSkillExpertise hasSkillExpertise && hasSkillExpertise.Skill == Skill && ProficiencyType == EProficiencyType.Expertise)
+        else if (command is HasSkillProficiency hasSkillProficiency)
         {
-            hasSkillExpertise.SetValue(this, true);
+            if (hasSkillProficiency.Skill == Skill && ProficiencyType == EProficiencyType.FullProficiency)
+            {
+                hasSkillProficiency.SetValue(this, true);
+            }
         }
-        else if (command is HasSkillHalfProficiency hasSkillHalfProficiency && hasSkillHalfProficiency.Skill == Skill && ProficiencyType == EProficiencyType.HalfProficiency)
+        else if (command is HasSkillExpertise hasSkillExpertise)
         {
-            hasSkillHalfProficiency.SetValue(this, true);
+            if (hasSkillExpertise.Skill == Skill && ProficiencyType == EProficiencyType.Expertise)
+            {
+                hasSkillExpertise.SetValue(this, true);
+            }
+        }
+        else if (command is HasSkillHalfProficiency hasSkillHalfProficiency)
+        {
+            if (hasSkillHalfProficiency.Skill == Skill && ProficiencyType == EProficiencyType.HalfProficiency)
+            {
+                hasSkillHalfProficiency.SetValue(this, true);
+            }
         }
         else if (command is GetSkillModifier getSkillModifier && getSkillModifier.Skill == Skill)
         {
+            if (IsOverridden)
+            {
+                return;
+            }
+
             var getProficiencyBonus = new GetProficiencyBonus(getSkillModifier.Character);
             var proficiencyBonusResult = getProficiencyBonus.Execute();
 
@@ -47,10 +88,5 @@ public class SkillProficiency : AFeat
                 getSkillModifier.SetErrorAndReturn("Couldn't get proficiency bonus: " + proficiencyBonusResult.ErrorMessage);
             }
         }
-    }
-
-    public void ChangeProficiencyLevel(EProficiencyType newProficiencyLevel)
-    {
-        ProficiencyType = newProficiencyLevel;
     }
 }
---
+++ src/Dnd.System/CommandSystem/Commands/EventCommands/AddLevel.cs
@@ -0,0 +1,24 @@
+﻿namespace Dnd.System.CommandSystem.Commands.EventCommands;
+
+using Dnd.System.Entities.Characters;
+using Dnd.System.Entities.Levels;
+
+public class AddLevel : DndEventCommand
+{
+    public AddLevel(IEventListener eventListener, ICharacter character, ILevel level) : base(eventListener, character)
+    {
+        Level = level;
+    }
+
+    public ILevel Level { get; }
+
+    protected override void InitializeEvent()
+    {
+    }
+
+    protected override void FinalizeEvent()
+    {
+        Character.LevelInfo.AddLevel(Level);
+        EventResult.SetMessage($"{Character.Name} has become {Level.Name}");
+    }
+}
